e1b5d655177ba57735b9ad143d1b40b9a1de8b07
Dilip <dilip@kryptstreet.com>
2022-03-30 01:08:33 +0530

    Firebase integration in progress

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/screens/widgets/custom_button.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({ Key? key }) : super(key: key);
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+        body: Container(
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Start or Join a meeting",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Image.asset("assets/images/onboarding.jpeg"),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 40),
+            child: CustomButton(
+              text: "Login",
+              pressAction: () {
+                print("Hello, world!!!");
+              },
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
